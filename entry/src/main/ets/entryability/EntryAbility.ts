1b009f6cbe91ed054e14338459a4a024ce68c91c
liushu <liushu27@h-partners.com>
2024-07-20 17:00:53 +0800

    修改登录

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,11 +1,11 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
-import referencesUtils from '../common/utils/common/PrefUtil'
+import PreferencesUtils from '../common/utils/common/PrefUtil'
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
-    referencesUtils.createPreferences(this.context)
+    PreferencesUtils.createPreferences(this.context)
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
 
@@ -16,8 +16,13 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
-    windowStage.loadContent('pages/login/LoginPage', (err, data) => {
+    let path = 'pages/login/LoginPage'
+    const token = PreferencesUtils.get('token') as unknown as string
+    hilog.info(0x0000, 'tokenVal', 'tokenVal %{public}s', token);
+    if (token && token.length) {
+      path = 'pages/Index'
+    }
+    windowStage.loadContent(path, (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
